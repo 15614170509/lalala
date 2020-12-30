cad94ec6c8a7bda8307316b8ac821c89b3c3ad80
shiyanlong <349028528@qq.com>
2020-12-30 17:11:38 +0800

    lalala的第一次提交 v1.0

---
+++ src/main/java/com/baizhi/lalala/LalalaApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.lalala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalalaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LalalaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baizhi/lalala/LalalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.lalala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LalalaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
